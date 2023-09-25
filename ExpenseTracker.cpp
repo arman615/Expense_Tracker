84cadd48d926fe07cde05f88149821e45ccbe211
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 22:54:11 +0400

    Update ExpenseTracker.cpp

--- ExpenseTracker.cpp
+++ ExpenseTracker.cpp
@@ -19,21 +19,67 @@ void ExpenseTracker::addExpense(const std::string& category, double amount, cons
 }
 
 void ExpenseTracker::printAllExpenses() const {
-    // Implement printAllExpenses as before
+    std::cout << "Expense List:\n";
+    for (const Expense& expense : expenses) {
+        std::cout << "Category: " << expense.getCategory() << std::endl;
+        std::cout << "Amount: $" << expense.getAmount() << std::endl;
+        std::cout << "Description: " << expense.getDescription() << std::endl;
+        std::cout << "Date/Time: " << expense.getDateTime() << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 void ExpenseTracker::generateCategorySummaries() const {
-    // Implement generateCategorySummaries as before
+    std::cout << "Category Summaries:\n";
+    for (const std::string& category : categories) {
+        double totalAmount = 0.0;
+        for (const Expense& expense : expenses) {
+            if (expense.getCategory() == category) {
+                totalAmount += expense.getAmount();
+            }
+        }
+        std::cout << "Category: " << category << std::endl;
+        std::cout << "Total Amount: $" << totalAmount << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 void ExpenseTracker::generateWeeklyReport() const {
-    // Implement generateWeeklyReport as before
+    std::cout << "Weekly Report:\n";
+    std::cout << "=========================\n";
+    for (const Expense& expense : expenses) {
+        std::cout << "Category: " << expense.getCategory() << std::endl;
+        std::cout << "Amount: $" << expense.getAmount() << std::endl;
+        std::cout << "Description: " << expense.getDescription() << std::endl;
+        std::cout << "Date/Time: " << expense.getDateTime() << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 void ExpenseTracker::generateMonthlyReport() const {
-    // Implement generateMonthlyReport as before
+    std::cout << "Monthly Report:\n";
+    std::cout << "=========================\n";
+    for (const Expense& expense : expenses) {
+        std::cout << "Category: " << expense.getCategory() << std::endl;
+        std::cout << "Amount: $" << expense.getAmount() << std::endl;
+        std::cout << "Description: " << expense.getDescription() << std::endl;
+        std::cout << "Date/Time: " << expense.getDateTime() << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 void ExpenseTracker::saveReportsToFile() const {
-    // Implement saveReportsToFile as before
+    std::ofstream file("expense_reports.txt");
+    if (file.is_open()) {
+        file << "Weekly Report:\n";
+        file << "=========================\n";
+        for (const Expense& expense : expenses) {
+            file << "Category: " << expense.getCategory() << std::endl;
+            file << "Amount: $" << expense.getAmount() << std::endl;
+            file << "Description: " << expense.getDescription() << std::endl;
+            file << "Date/Time: " << expense.getDateTime() << std::endl;
+            file << std::endl;
+        }
+        file.close();
+    }
 }
