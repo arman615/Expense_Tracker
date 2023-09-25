809fbed26f4907c2cca9e2f5d8c444f57caeab4b
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 22:54:41 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include "Expense.h"
+#include "ExpenseTracker.h"
 
+int main() {
+    ExpenseTracker expenseTracker;
+    expenseTracker.addExpense("Food", 20.5, "Lunch", "2023-07-01 12:30");
+    expenseTracker.addExpense("Transportation", 15.0, "Bus fare", "2023-07-02 08:45");
+    expenseTracker.addExpense("Shopping", 50.75, "Clothes", "2023-07-02 15:20");
+    expenseTracker.addExpense("Food", 12.75, "Dinner", "2023-07-03 19:00");
+    expenseTracker.addExpense("Utilities", 30.0, "Electricity bill", "2023-07-04 10:15");
+    expenseTracker.printAllExpenses();
+    expenseTracker.generateCategorySummaries();
+    expenseTracker.generateWeeklyReport();
+    expenseTracker.generateMonthlyReport();
+    expenseTracker.saveReportsToFile();
+    return 0;
+}
